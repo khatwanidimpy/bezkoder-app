c683db068ecd8bcf0c5003e16e53ee9dc7e4e570
dimpy khatwani <khatwanidimpy@gmail.com>
2026-05-04 01:08:54 +0530

    Configure Prometheus to scrape Node.js app metrics on ports 3000 and 8080

--- bezkoder-app/server.js
+++ bezkoder-app/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
+const client = require("prom-client");
 
 const app = express();
+
+client.collectDefaultMetrics();
 
 var corsOptions = {
   origin: "http://localhost:8081"
@@ -9,10 +12,8 @@ var corsOptions = {
 
 app.use(cors(corsOptions));
 
-// parse requests of content-type - application/json
 app.use(express.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
 const db = require("./app/models");
@@ -24,19 +25,17 @@ db.sequelize.sync()
     console.log("Failed to sync db: " + err.message);
   });
 
-// // drop the table if it already exists
-// db.sequelize.sync({ force: true }).then(() => {
-//   console.log("Drop and re-sync db.");
-// });
-
-// simple route
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to bezkoder application." });
 });
 
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
 require("./app/routes/turorial.routes")(app);
 
-// set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
