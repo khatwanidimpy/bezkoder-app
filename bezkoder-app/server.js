65ca1b778e67097ca35f289b9446e3bbd68bbdbd
dimpy khatwani <khatwanidimpy@gmail.com>
2026-05-04 01:18:57 +0530

    Fix Prometheus monitoring by exposing /metrics and binding server to 0.0.0.0

--- bezkoder-app/server.js
+++ bezkoder-app/server.js
@@ -37,6 +37,7 @@ app.get("/metrics", async (req, res) => {
 require("./app/routes/turorial.routes")(app);
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}.`);
 });
